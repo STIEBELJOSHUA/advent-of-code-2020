5f6ae0f7504c0e8b2bdb3182de78f6ece3cc06da
Josh S <45885649+STIEBELJOSHUA@users.noreply.github.com>
2020-12-11 03:25:53 -0500

    finished 10

--- AOC 2020/advent10.py
+++ AOC 2020/advent10.py
@@ -1,17 +1,36 @@
+import sys
+import functools
+
+
+sys.setrecursionlimit(1000000000)
+
+
 with open ('input.txt','r') as file:
 	adapters = [int(x) for x in file.read().strip().split()]
+	adapters.append(0)
+	adapters.append(max(adapters)+3)
 	adapters.sort()
 	
 joltages = []
-for i in range(0,len(adapters)):
-	if i == 0:
-		joltages.append(adapters[i])
-	else:
+for i in range(1,len(adapters)):
 		joltages.append(adapters[i]-adapters[i-1])
 
-silver = joltages.count(1) * (joltages.count(3)+1)
+silver = joltages.count(1) * (joltages.count(3))
+
+
+@functools.lru_cache(maxsize=None)
+def find_permutations(i,list):
+	permutations=0
+	if i >= len(list)-2:
+		permutations += 1
+	elif i == len(list)-3:
+		for x in [j for j in range(1,3) if adapters[i+j] <= adapters[i]+3]:
+			permutations += find_permutations(x+i,list)
+	else:
+		for x in [j for j in range(1,4) if adapters[i+j] <= adapters[i]+3]: 
+			permutations += find_permutations(x+i,list)
+	return permutations
+
 print(silver)
-
-
-
-	
+gold = find_permutations(0,tuple(adapters))
+print(gold)
